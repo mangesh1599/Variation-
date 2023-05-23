a5d091ed8d6b05512a545832f497108e2abcb8f2
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:58:09 +0530

    Create Print One Small One Large Number.java

---
+++ Print One Small One Large Number.java
@@ -0,0 +1,34 @@
+package variation;
+
+import java.util.Arrays;
+
+public class small_large {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int []a= {2,4,3,1,5,8,9,6,7};
+		Arrays.sort(a);
+
+		int []b=new int[a.length];
+		int i=0;
+		int j=a.length-1;
+		int index=0;
+		while(i<j)
+		{
+			b[index++]=a[i];
+			b[index++]=a[j];
+
+			i++;
+			j--;
+		}
+		if(i==j)
+		{
+			b[index]=a[i];
+		}
+		for(int data:b)
+		{
+			System.out.print(data+" ");
+		}
+	}
+}
