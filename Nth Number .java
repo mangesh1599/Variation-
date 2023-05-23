9d971e4ac710615a1450db32425fa993bc368418
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:26:09 +0530

    Create Nth Number .java

---
+++ Nth Number .java
@@ -0,0 +1,40 @@
+package variation;
+
+public class nth_number{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int n=10;
+		int i=1;
+		int cnt=0;
+		while(true)
+		{
+			int num=i;
+			while(num>0)
+			{
+				int digit=num%10;
+				if(digit==3 || digit==4)
+				{
+					num=num/10;
+				}
+				else
+				{
+					break;
+				}
+			}
+			if(num==0)
+			{
+				cnt++;
+				System.out.println(i);
+			}
+			if(cnt==n)
+			{
+				break;
+			}
+			i++;
+		}
+	}
+}
+
+
