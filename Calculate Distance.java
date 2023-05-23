ff6ebfdf952708249ecb7c216f2d18e22fc96f55
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:41:07 +0530

    Create Calculate Distance.java

---
+++ Calculate Distance.java
@@ -0,0 +1,13 @@
+package variation;
+
+public class calculate_distance {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int x1=1,x2=4,y1=1,y2=4;
+
+		System.out.println(Math.sqrt(Math.pow((x1-x2), 2)+Math.pow((y1-y2), 2))*100000.0/100000.0);
+	}
+
+}
