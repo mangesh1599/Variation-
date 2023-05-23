5fe3810f60d0df7cbff91b76d567d016d3d48314
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:46:37 +0530

    Create REminder 7 .java

---
+++ REminder 7 .java
@@ -0,0 +1,19 @@
+package variation;
+
+public class remainder_7 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s="12345";
+		int num=0;
+		int rem=0;
+		for(int i=0;i<s.length();i++)
+		{
+			num=rem*10+(s.charAt(i)-48);		
+			rem=num%7;
+		}
+		System.out.println(rem);
+
+	}
+}
