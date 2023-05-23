beee8187ab3d36c903f1e800d232530c0956fab9
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:21:38 +0530

    Create Maximize Sum .java

---
+++ Maximize Sum .java
@@ -0,0 +1,21 @@
+package variation;
+
+import java.util.Arrays;
+
+public class maximize_sum {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int []a= {1,4,3};
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+			sum=sum+a[i]*i;
+
+		}
+		System.out.println(sum);
+
+	}
+
+}
