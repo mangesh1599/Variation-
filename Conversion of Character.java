acd336a4a8296cc2c9e09bb5c9405409092f8775
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:46:00 +0530

    Create Conversion of Character.java

---
+++ Conversion of Character.java
@@ -0,0 +1,27 @@
+package variation;
+
+public class conversion_of_character {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s1="VauN";
+		String s2="";
+		for(int i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)>='a' && s1.charAt(i)<='z')
+			{
+				char ch=(char)(219-s1.charAt(i));
+				s2=s2+ch;
+			}
+			else
+			{
+				char ch=(char)(155-s1.charAt(i));
+				s2=s2+ch;
+			}
+		}
+		System.out.println(s2);
+
+	}
+
+}
