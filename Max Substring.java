d6cbd7a17d2a8df475abbd1a5ffbdd8ff8bbba77
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:12:53 +0530

    Create Max Substring.java

---
+++ Max Substring.java
@@ -0,0 +1,21 @@
+package variation;
+
+public class max_substring {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s="11100011110001";
+		int max=Integer.MIN_VALUE;
+
+		String []s2=s.split("0");
+		for(int i=0;i<s2.length;i++)
+		{
+			if(s2[i].length()>max)
+			{
+				max=s2[i].length();
+			}
+		}
+		System.out.println(max);
+	}
+}
