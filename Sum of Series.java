c790153c36bc48cee2cbbc0252ed9e8f4f2bbe30
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 16:08:06 +0530

    Create Sum of Series.java

---
+++ Sum of Series.java
@@ -0,0 +1,21 @@
+package variation;
+
+public class sum_of_series {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int n=5;
+		int a=1;
+		int sum=1;
+		for(int i=2;i<=n;i++)
+		{
+			a=a+i;
+			sum=sum+a;
+
+		}
+		System.out.println("sum : "+sum);
+
+	}
+
+}
