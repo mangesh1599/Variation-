7eaa57aff1961b0c91d1d6061e477fa9b74c94a3
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:56:51 +0530

    Create Shift All Zero's Last.java

---
+++ Shift All Zero's Last.java
@@ -0,0 +1,29 @@
+package variation;
+
+public class shift_all_zero_last {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a[]= {0,2,4,6,0,2,-3,0,0};
+		int []b =new int [a.length];
+		int ind=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]!=0)
+			{
+				b[ind++]=a[i];
+			}
+		}
+		for (int i = 0; i < a.length; i++) 
+		{
+			if(a[i]==0)
+				b[ind++]=a[i];
+		}
+		for (int i = 0; i < b.length; i++) 
+		{
+			System.out.print(b[i]+" ");
+		}
+	}
+
+}
