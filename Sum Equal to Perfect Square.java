6ece56e77055f561f9502dc149c7ecd6f2cbfd06
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 16:05:25 +0530

    Create Sum Equal to Perfect Square.java

---
+++ Sum Equal to Perfect Square.java
@@ -0,0 +1,32 @@
+package variation;
+
+public class sum_equal_perfect_square {
+
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int n=12;
+		int i=1;
+		int sum=0;
+		boolean b = false;
+		while(i<=n)
+		{
+			sum=sum+i;
+			i=i+2;
+			if(sum==n)
+			{
+				b=true;
+			}
+		}
+		if(b==true)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+
+}
