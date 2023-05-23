28dbf36ddb0f0d81bcf236f238ea76997f7a54ca
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:45:47 +0530

    Create Print Unique Character in String.java

---
+++ Print Unique Character in String.java
@@ -0,0 +1,32 @@
+package variation;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class print_unique_character_in_string {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String a="geeksforgeeks";
+		String b="geeksquiz";
+
+		String s3="";
+		TreeSet<Character>ts=new TreeSet<Character>();
+		for(int i=0;i<a.length();i++) 
+		{
+			if(!b.contains(a.charAt(i)+""))
+			{
+				ts.add(a.charAt(i));
+			}
+		}
+		for(int i=0;i<b.length();i++) 
+		{
+			if(!a.contains(b.charAt(i)+""))
+			{
+				ts.add(b.charAt(i));
+			}
+		}
+		System.out.println(ts);
+	}
+}
