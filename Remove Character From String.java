efaaba213dbeb474b9c4264278e85e7b560d737b
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:48:02 +0530

    Create Remove Character From String.java

---
+++ Remove Character From String.java
@@ -0,0 +1,36 @@
+package variation;
+
+public class remove_charcater_from_string {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s1="computer";
+		String s2="cat";
+
+		String s3="";
+		if(s1.length()>s2.length())
+		{
+			for(int i=0;i<s1.length();i++)
+			{
+				int cnt=0;
+				for(int j=0;j<s2.length();j++)
+				{
+					if(s1.charAt(i)==s2.charAt(j))
+					{
+						cnt++;
+					}
+				}
+				if(cnt==0)
+				{
+					s3=s3+s1.charAt(i);
+				}
+			}
+			System.out.print(s3);
+		}
+		else
+		{
+			System.out.println(-1);
+		}
+	}
+}
