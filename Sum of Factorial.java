0f896b816453bc69db3e77d60000c520318ba078
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 16:07:30 +0530

    Create Sum of Factorial.java

---
+++ Sum of Factorial.java
@@ -0,0 +1,41 @@
+package variation;
+
+public class sum_of_factorial {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int num=145;
+		int sum=0;
+		int temp=num;
+		int fact=1;
+		while(num>0)
+
+		{
+			int rem=num%10;
+			int myfact=factorial(rem);
+			sum=sum+myfact;
+			num=num/10;
+		}
+		if(sum==temp)
+		{
+			System.out.println("strong");
+		}
+		else
+		{
+			System.out.println("weak");
+		}
+
+	}
+	private static int factorial(int rem) 
+	{
+		int fact=1;
+
+		for(int i=1;i<=rem;i++)
+		{
+			fact=fact*i;
+
+		}
+		return fact;
+	}
+}
