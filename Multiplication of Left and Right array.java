0e97f9ee252ee042b1a2bf2c1604eeb6075d243e
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:22:52 +0530

    Create Multiplication of Left and Right array.java

---
+++ Multiplication of Left and Right array.java
@@ -0,0 +1,26 @@
+package variation;
+
+public class multiplication_of_left_and_right_array {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a[]= {1,4,6,8};
+
+		int mid=a.length/2;
+
+		int leftsum=0;
+		for(int i=0;i<mid;i++)
+		{
+			leftsum+=a[i];
+		}
+
+		int rightsum=0;
+		for(int i=mid;i<a.length;i++)
+		{
+			rightsum+=a[i];
+		}
+		System.out.println(leftsum*rightsum);
+	}
+
+}
