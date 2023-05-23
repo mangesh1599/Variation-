1a0de4b4924741ff54a674fcf9a83c59fdb8af9b
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:04:23 +0530

    Create Finger Problem.java

---
+++ Finger Problem.java
@@ -0,0 +1,30 @@
+package variation;
+
+public class finger_prbm {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int n=20;
+
+		int finger=n%8;
+
+		if(finger==1)
+			System.out.println("thumb finger");
+
+		if(finger==0 || finger==2)
+			System.out.println("index finger");
+
+		if(finger==3 || finger==7)
+			System.out.println("middle finger");
+
+		if(finger==4 || finger==6)
+			System.out.println("ring finger");
+
+		if(finger==5)
+			System.out.println("little finger");
+
+
+	}
+
+}
