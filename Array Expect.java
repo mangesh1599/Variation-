c776cdbda33d04810f59634c6827544f971a7ffb
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:37:02 +0530

    Create Array Expect.java

---
+++ Array Expect.java
@@ -0,0 +1,31 @@
+package variation;
+
+public class array_Except {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int []a= {77,80,-17,19,20};
+		int cnt=0;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==0 || a[i]==-1)
+			{
+				cnt++;
+
+			}
+		}
+		if(cnt==0)
+		{
+			System.out.println(true);
+
+		}
+		else
+		{
+			System.out.println(false);
+
+		}
+	}
+
+}
