d707bf753429c519bb59121bf691df7895213846
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 16:15:09 +0530

    Create Swipe K Element.java

---
+++ Swipe K Element.java
@@ -0,0 +1,21 @@
+package variation;
+
+public class swipe_k_ele {
+
+	public static void main(String[] args) {
+		int[] arr = {5,1,4,3,2};
+		for (int data: arr) {
+			System.out.print(data+" ");
+		}
+
+		int temp = arr[1];
+		arr[1] = arr[3];
+		arr[3] = temp;
+		System.out.println();
+		for (int data: arr) {
+			System.out.print(data+" ");
+		}
+
+	}
+
+}
