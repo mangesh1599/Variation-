7718c2f8d26bad4f23579c90fcd92ae0d01af01d
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:40:31 +0530

    Create Balanced Array.java

---
+++ Balanced Array.java
@@ -0,0 +1,28 @@
+package variation;
+
+public class balanced_array {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		int a[]= {1,5,3,2};
+
+		int leftsum=0;
+		int rightsum=0;
+		int mid=a.length/2;
+
+		int min=Integer.MAX_VALUE;
+
+		for(int i=0;i<mid;i++)
+		{
+			leftsum=leftsum+a[i];
+		}
+		for(int j=mid;j<a.length;j++)
+		{
+			rightsum=rightsum+a[j];
+		}
+		int diff=Math.abs(rightsum-leftsum);
+		System.out.println(diff);
+	}
+}
