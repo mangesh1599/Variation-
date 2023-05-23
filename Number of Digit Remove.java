76f6fc8cd8b417a772dbc410d1a059fe3572741a
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:27:21 +0530

    Create Number of Digit Remove.java

---
+++ Number of Digit Remove.java
@@ -0,0 +1,41 @@
+package variation;
+
+public class number_of_digit_to_remove {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		String s="11";
+		int sum=0;
+		if(s.length()==1)
+		{
+			System.out.println(-1);
+		}
+		if(s.length()==2)
+		{
+			System.out.println(-1);
+		}
+		else
+		{
+			for(int i=0;i<s.length();i++)
+			{
+				sum=sum+(int)s.charAt(i);
+			}
+			for(int i=0;i<s.length();i++)
+			{
+				if(sum%3==(s.charAt(i)-48)%3)
+				{
+					System.out.println(1);
+					break;
+				}
+				else
+				{
+					System.out.println(-1);
+					break;
+				}
+			}
+
+		}
+	}
+}
