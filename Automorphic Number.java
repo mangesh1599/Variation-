793b7a8eaf2a2b192803f8d99ee1986053ef789e
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:39:19 +0530

    Create Automorphic Number.java

---
+++ Automorphic Number.java
@@ -0,0 +1,20 @@
+package variation;
+
+public class automorphic_number {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int num=5;
+
+		int sqrt=num*num;
+
+		String s1=Integer.toString(sqrt);
+		String s2=Integer.toString(num);
+
+
+		System.out.println(s1.endsWith(s2));
+
+	}
+
+}
