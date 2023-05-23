a37f4d4ed7c82739448b90b9ae075f7ef6417e2f
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:50:58 +0530

    Create Discreminent Value.java

---
+++ Discreminent Value.java
@@ -0,0 +1,22 @@
+package variation;
+
+public class Discremementvalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=30;
+		int c=10;
+
+		int d=(b*b)-(4*a*c);
+
+		if(d>0)
+		{
+			System.out.println("positive");
+
+		}
+		else
+			System.out.println("negative");
+	}
+
+}
