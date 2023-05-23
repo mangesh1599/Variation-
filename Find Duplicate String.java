2aa0e60ecdf6b2d1e4975aad3c921f788ac54d13
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:59:23 +0530

    Create Find Duplicate String.java

---
+++ Find Duplicate String.java
@@ -0,0 +1,33 @@
+package variation;
+
+public class find_duplicate_string {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stuab
+		String s1="Big black bug bit a big black dog on his big black noise";
+
+
+		String[] s2=s1.split(" ");
+
+		String s3="";
+		for(int i=0;i<s2.length;i++)
+		{
+			int cnt=0;
+			for(int j=i+1;j<s2.length;j++)
+			{
+				if(s2[i].equals(s2[j]))
+				{
+					cnt++;
+
+				}
+
+			}
+			if(cnt==1)
+
+				System.out.print(s2[i]+" ");
+
+		}
+	}
+
+}
