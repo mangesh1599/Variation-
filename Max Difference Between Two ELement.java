c289586d21dc1fda29b0f89b6c858f963dac35d2
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:11:32 +0530

    Create Max Difference Between Two ELement.java

---
+++ Max Difference Between Two ELement.java
@@ -0,0 +1,23 @@
+package variation;
+
+public class max_distence_betn_two_ele {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a[]= {3,2,1,2,1,4,5,8,6,7,4,2};
+
+		int max=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]==a[j] && max<j-i)
+				{
+					max=j-i;
+				}
+			}
+		}
+		System.out.println(max);
+	}
+}
