aa2a9d2a8445d4d33bf3986e3539c7f16eb7703e
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 15:09:35 +0530

    Create Make Element Distinct.java

---
+++ Make Element Distinct.java
@@ -0,0 +1,27 @@
+package variation;
+
+public class make_ele_distinct {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int a[]= {2,2,2,3,5,6};
+		int sum=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				if(a[i]==a[j])
+				{
+					a[i]=a[i]+1;
+				}
+			}
+			//System.out.print(a[i]+" ");
+			sum=sum+a[i];
+		}
+		System.out.println(sum);
+
+
+	}
+
+}
