c62efcfb7b105346f1ec64d110b634cb268fb42b
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-23 14:48:24 +0530

    Create Delete Alternative Charcater.java

---
+++ Delete Alternative Charcater.java
@@ -0,0 +1,16 @@
+package variation;
+
+public class delete_alternative_character {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s1="hefshine";
+		for(int i=0;i<s1.length();i=i+2)
+		{
+			System.out.print(s1.charAt(i));
+		}
+
+	}
+
+}
