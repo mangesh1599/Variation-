12a3333ad37ed71bb0530be6c57bcbe04d5cb9a1
Mangesh Dalvi <84315964+mangesh1599@users.noreply.github.com>
2023-05-26 18:01:03 +0530

    Create Cumulative frequency of count of each element in an unsorted array.java

---
+++ Cumulative frequency of count of each element in an unsorted array.java
@@ -0,0 +1,54 @@
+Given an array of elements. The task is to calculate the cumulative frequency of each element of the array.
+
+Input : Arr[] = {1, 2, 2, 1, 3, 4}
+Output : 2 4 5 6
+Explanation:
+Here we have an array arr[] = [1, 3, 2, 
+                               1, 2, 4]
+Output :1->2
+        2->4
+        3->5
+        4->6
+So, the return array will have [2, 4, 5, 6] 
+as an output.
+  
+  
+  
+  
+  
+ 
+  public class MyClass 
+{
+	//StartOfMainMethod
+	public static void main(String[] args) 
+	{
+		int[] a= 
+		{
+			1, 2, 2, 1, 3, 4
+		}
+		;
+		int n=a.length;
+		myCode(a,n);	
+	}
+	//EndOfMainMethod
+	public static void myCode(int[] a,int n) 
+	{
+		// write code here
+		int index=0;
+		int b[] = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			b[index++]=a[i];
+		}
+		int freq = 0;
+		for(int i = 0; i < n; i++)
+		{
+			freq += b[a[i]];
+			if(b[a[i]] != 0)
+			{
+				System.out.println(a[i] + "->" + freq);
+			}
+			b[a[i]] = 0;
+		}
+	}
+}
